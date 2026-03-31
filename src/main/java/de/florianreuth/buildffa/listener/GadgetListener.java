dc27a88d87d5417d9130db842dfa11b8cd5247cf
yyuh <dustinohlemann@gmail.com>
2026-03-31 20:07:53 +0200

    Fix gadget not working when right-clicking air
    Co-authored-by: Florian Reuth <git@florianreuth.de>
    Signed-off-by: Florian Reuth <git@florianreuth.de>

--- src/main/java/de/florianreuth/buildffa/listener/GadgetListener.java
+++ src/main/java/de/florianreuth/buildffa/listener/GadgetListener.java
@@ -1,18 +1,18 @@
 package de.florianreuth.buildffa.listener;
 
+import de.florianreuth.buildffa.service.BuildService;
 import de.florianreuth.buildffa.service.GadgetService;
 import de.florianreuth.buildffa.service.GadgetService.GadgetDefinition;
-import de.florianreuth.buildffa.service.BuildService;
 import java.util.Optional;
 import net.kyori.adventure.text.Component;
 import net.kyori.adventure.text.format.NamedTextColor;
 import org.bukkit.entity.Player;
-import org.bukkit.inventory.EquipmentSlot;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.EventPriority;
 import org.bukkit.event.Listener;
-import org.bukkit.event.block.Action;
 import org.bukkit.event.player.PlayerInteractEvent;
+import org.bukkit.inventory.EquipmentSlot;
+import org.bukkit.inventory.ItemStack;
 
 public final class GadgetListener implements Listener {
 
@@ -30,12 +30,13 @@ public final class GadgetListener implements Listener {
             return;
         }
 
-        Action action = event.getAction();
-        if (action != Action.RIGHT_CLICK_AIR && action != Action.RIGHT_CLICK_BLOCK) {
+        final Player player = event.getPlayer();
+
+        final ItemStack handItem = player.getInventory().getItemInMainHand();
+        if (handItem.isEmpty() || handItem.getType().isAir()) {
             return;
         }
 
-        Player player = event.getPlayer();
         if (buildService.isInSpawnProtection(player.getLocation())) {
             event.setCancelled(true);
             player.sendActionBar(Component.text("You cannot use items near spawn.", NamedTextColor.RED));
@@ -61,5 +62,5 @@ public final class GadgetListener implements Listener {
             event.setCancelled(true);
         }
     }
-}
 
+}
